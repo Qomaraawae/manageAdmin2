39421645002179702023a8af8e71a86d79484983
Qomaraawae <141138947+Qomaraawae@users.noreply.github.com>
2025-04-25 00:31:10 +0700

    Update HomeComponent.js

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -8,11 +8,14 @@ function HomeComponent() {
     <div className="min-h-screen flex flex-col items-center justify-center text-white px-4 text-center space-y-6">
       <h1 className="text-4xl md:text-5xl font-bold tracking-wide text-black">
         Selamat Datang di
-        <span className="block text-blue-600">Aplikasi Laporan Barang Hilang</span>
+        <span className="block text-blue-600">
+          Aplikasi Laporan Barang Hilang
+        </span>
       </h1>
 
       <p className="text-lg text-gray-700 max-w-lg">
-        Laporkan barang yang hilang atau temukan barang yang hilang dengan mudah dan cepat.
+        Laporkan barang yang hilang atau temukan barang yang hilang dengan mudah
+        dan cepat.
       </p>
 
       <div className="flex flex-col md:flex-row gap-4">
@@ -21,12 +24,6 @@ function HomeComponent() {
           className="px-8 py-3 text-lg font-semibold bg-blue-500 rounded-lg shadow-lg hover:bg-blue-600 transform hover:scale-105 transition duration-300 text-white"
         >
           Laporan Hilang
-        </button>
-        <button
-          onClick={() => navigate("/laporan-ditemukan")}
-          className="px-8 py-3 text-lg font-semibold bg-teal-500 rounded-lg shadow-lg hover:bg-teal-600 transform hover:scale-105 transition duration-300 text-white"
-        >
-          Laporan Ditemukan
         </button>
       </div>
     </div>
